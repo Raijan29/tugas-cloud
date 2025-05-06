2e56f87398845168407cc0c8d1df55cd61596efc
sastrapradana <bp846622@gmail.com>
2025-05-06 22:16:50 +0700

    fitur: form booking

--- src/components/appshell.jsx
+++ src/components/appshell.jsx
@@ -1,10 +1,12 @@
+import Footer from "./footer";
 import Headers from "./headers";
 
-export default function Appshell ({children}) {
-    return (
-        <div className="min-h-screen flex flex-col">
-            <Headers />
-            {children}
-        </div>
-    ) 
+export default function Appshell({ children }) {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Headers />
+      {children}
+      <Footer />
+    </div>
+  );
 }
